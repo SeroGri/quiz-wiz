966c283517bc584d35d93bbadd75f8c9f15bbbfd
SeroGri <sero.grigoryan@gmail.com>
2022-01-20 18:40:55 -0800

    base

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const startButton = document.querySelector('#start');
+const introText = document.querySelector('#intro');
+
+
+function startQuiz() {
+
+};
+
+
+//When the "startButton" is clicked, startQuiz();
+startButton.addEventListener("click", startQuiz, delete startButton);
+
